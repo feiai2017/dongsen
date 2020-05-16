db3ff95dac1bbfda7cb643247a3b44c47bc81f90
feiai2017 <274878379@qq.com>
2020-05-16 20:54:41 +0800

    条件分页  tmp

--- app/Http/Controllers/HandbookController.php
+++ app/Http/Controllers/HandbookController.php
@@ -7,16 +7,17 @@ use App\Models\Art;
 use App\Models\Bug;
 use App\Models\Fish;
 use App\Models\Fossil;
+use App\Models\Furniture;
 use Illuminate\Http\Request;
 
 class HandbookController extends Controller
 {
     //
-    public function getList($arg) {
+    public function getList(Request $request, $arg) {
         switch ($arg) {
             case 'animal':
                 $animal = new Animal();
-                $res = $animal->getList();
+                $res = $animal->getList($request);
                 break;
             case 'fish':
                 $fish = new Fish();
@@ -33,6 +34,10 @@ class HandbookController extends Controller
             case 'fossil':
                 $fossil = new Fossil();
                 $res = $fossil->getList();
+                break;
+            case 'furniture':
+                $furniture = new Furniture();
+                $res = $furniture->getList();
                 break;
         }
 
--- app/Models/Picture.php
+++ app/Models/Picture.php
@@ -3,17 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class Picture extends Model
 {
-    protected function getList() {
-        $res = Model::paginate();
+    protected function getList(Request $request) {
+        $where_array = array();
+
+        if (!empty($request['name']))
+            $where_array['name'] = $request['name'];
+
+        if (!empty($request['race']))
+            $where_array['race'] = $request['race'];
+
+        $res = Model::where($where_array)->paginate();
+        Log::debug('where_array: ' . json_encode($where_array, JSON_UNESCAPED_UNICODE));
+        Log::debug('res: ' . json_encode($res, JSON_UNESCAPED_UNICODE));
 
         $url = 'https://fly.sailoa.com/storage';
         foreach ($res as $key => $value){
             $val = substr($value->image, 2);
             $res[$key]['image'] = "$url$val";
         }
+
+        $where_array['page'] = isset($page)?$request['page']:1;
+        $res = $res->appends($where_array);
 
         return $res;
     }
